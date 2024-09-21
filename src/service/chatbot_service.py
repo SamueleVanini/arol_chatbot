6822666c2fcce28a4eecab9c88cc7f8a7c08b617
Amirali <a2developmentaccess@gmail.com>
2024-09-21 12:09:42 +0200

    - refactor code

--- src/service/chatbot_service.py
+++ src/service/chatbot_service.py
@@ -1,10 +1,9 @@
-from .llm_service import create_llm_model
+from core.config import configure_system
 from .file_loader_service import file_loader
 from .indexing_serivce import create_embeddings
+from .langchain_builder_service import LangChainBuilder, ChainType, MemoryType
+from .llm_service import create_llm_model
 from .retriever_service import create_vectorstore_retriever, get_history_aware_retriever
-from .langchain_builder_service import LangChainBuilder
-from .history_service import connect_history_session
-from core.config import configure_system
 
 
 class ArolChatBot:
@@ -15,12 +14,10 @@ class ArolChatBot:
         indexing = create_embeddings(docs=docs)
         retriever = create_vectorstore_retriever(indexing)
         history_aware_retriever = get_history_aware_retriever(llm=llm, retriever=retriever)
-        chain_builder = LangChainBuilder()
-        prompt_template = chain_builder.get_template(template_type="chat")
-        rag_chain = chain_builder.create_rag_chain(llm=llm, retriever=history_aware_retriever, prompt=prompt_template)
-        rag_chain_with_parser = rag_chain | chain_builder.response_parser
-        history_aware_chain = chain_builder.create_history_aware_chain(
-            chain=rag_chain_with_parser,
-            history_session=connect_history_session
-        )
-        return history_aware_chain
+        # Initialize the builder with desired options
+        chain_builder = LangChainBuilder(chain_type=ChainType.CHAT, memory_type=MemoryType.REDIS)
+
+        # Build the chain
+        final_chain = chain_builder.build_chain(llm, history_aware_retriever)
+
+        return final_chain
