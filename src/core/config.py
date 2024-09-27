4b23cf3d0774ef4f8b2a8e1818a178191ba7d3a9
Svanni <samuele.vanini@live.it>
2024-09-27 14:55:50 +0200

    update self-querying and config

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,11 @@
-import os
+from dotenv import load_dotenv
 
-REDIS_URL: str = \
+# this should be moved in the .env file too
+REDIS_URL: str = (
     "redis://default:QWoHMeuRDr3d0Gn0iqvUDAeasUIeqkB3@redis-19112.c339.eu-west-3-1.ec2.redns.redis-cloud.com:19112"
+)
 
 
 def configure_system():
-    os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-    os.environ['LANGCHAIN_API_KEY'] = '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"'
-    os.environ["GROQ_API_KEY"] = "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"
+    """Entry point to configure the running system, add in the body of the function all the logic to configure the chatbot"""
+    load_dotenv()
--- src/query_construction/self_querying.py
+++ src/query_construction/self_querying.py
@@ -3,6 +3,7 @@ from langchain.chains.query_constructor.schema import AttributeInfo
 # TODO: check if we should use space or underscore for the name of the Attribute (e.g. bottles per hour vs. bottles_per_hour)
 
 # Info about the metadata associated with each document in the vectorstore, it is used as a context for the llm in self-querying
+# NOTE: Expected metadata value to be a str, int, float or bool. Otherwise Chroma will raise a ValueError exception
 METADATA_FIELD_INFO = [
     AttributeInfo(
         name="name",
@@ -12,12 +13,12 @@ METADATA_FIELD_INFO = [
     AttributeInfo(
         name="bottles_per_hour",
         description="The number of closed bottles every hour",
-        type="list of integer",
+        type="integer",
     ),
     AttributeInfo(
         name="bottles_per_minute",
         description="The number of closed bottles every minute",
-        type="list of integer",
+        type="integer",
     ),
     # AttributeInfo(name="caps application", description="The type of caps usable by the machine", type="string"),
 ]
@@ -36,13 +37,13 @@ def metadata_extraction(record: dict, metadata: dict) -> dict:
     for prod_speed_str in main_features.get("speed production"):
         # really ugly name but quite fitting (we are replacing the "." used to represent number in the Arol Catalog with "_")
         prod_speed_py_int_number = prod_speed_str.replace(".", "_")
-        minutes_split, hours_split = prod_speed_py_int_number.split(" ")
-        for word in minutes_split:
+        minutes_split, hours_split = prod_speed_py_int_number.split("/")
+        for word in minutes_split.split(" "):
             if "_" in word or word.isnumeric():
-                metadata["bottles_per_minute"].append(int(word))
+                metadata["bottles_per_minute"] = int(word)
 
-        for word in hours_split:
+        for word in hours_split.split(" "):
             if "_" in word or word.isnumeric():
-                metadata["bottles_per_hour"].append(int(word))
+                metadata["bottles_per_hour"] = int(word)
 
     return metadata
