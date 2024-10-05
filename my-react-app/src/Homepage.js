c44d372da92e91c3cf885087b6849fce4f0367bc
Greta Tumiatti <s332148@studenti.polito.it>
2024-10-05 09:20:05 +0200

    changes chat

--- my-react-app/src/Homepage.js
+++ my-react-app/src/Homepage.js
@@ -4,16 +4,13 @@ function Homepage() {
     return (
       <div className="homepage">
         <div className="border-homepage">
-          <div className="image-container">
             <img src="/th.jpeg" alt="AROL" className="welcome-image" />
-          </div>
           <div className="text-container">
-          <h1 className="title">AROL CHATBOT</h1>
           <form className="input-form">
-              <input type="text" placeholder="Email" className="input-field" />
-              <input type="text" placeholder="Password" className="input-field" />
-              <button type="submit" className="submit-button">Sign In</button>
-              <button type="submit" className="submit-button">Sign Up</button>
+          <input type="text" placeholder="Email" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <button type="button" className="submit-button">Sign In</button>
+          <button type="button" className="submit-button">Sign Up</button>
             </form>
           </div>
         </div>
