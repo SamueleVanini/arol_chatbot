227f51768a6680c5c54ca4a04d7f43e1669ca7f5
Amirali <a2developmentaccess@gmail.com>
2024-11-06 14:22:54 +0100

    - refactor - use persist Chroma

--- src/service/indexing_serivce.py
+++ src/service/indexing_serivce.py
@@ -1,19 +1,25 @@
-from langchain_chroma import Chroma
+from src.vector_stores.chroma import ChromaCollection
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def create_embeddings(docs, split=False):
     if split:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0) #chunk_size: The size of each chunk in characters
-        docs = text_splitter.split_documents(docs) #split_documents: Split a list of documents into chunks
-    model_name = "sentence-transformers/multi-qa-mpnet-base-dot-v1" #model_name: The name of the model to use
-    model_kwargs = {'device': 'cpu'}                                #model_kwargs: Keyword arguments to pass to the model
-    encode_kwargs = {'normalize_embeddings': True}              #encode_kwargs: Keyword arguments to pass to the encode method
-    hf_embeddings = HuggingFaceEmbeddings(                      #HuggingFaceEmbeddings: A class for using Hugging Face models
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,
+                                                       chunk_overlap=0)  # chunk_size: The size of each chunk in characters
+        docs = text_splitter.split_documents(docs)  # split_documents: Split a list of documents into chunks
+    model_name = "sentence-transformers/multi-qa-mpnet-base-dot-v1"  # model_name: The name of the model to use
+    model_kwargs = {'device': 'cpu'}  # model_kwargs: Keyword arguments to pass to the model
+    encode_kwargs = {'normalize_embeddings': True}  # encode_kwargs: Keyword arguments to pass to the encode method
+    hf_embeddings = HuggingFaceEmbeddings(  # HuggingFaceEmbeddings: A class for using Hugging Face models
         model_name=model_name,
         model_kwargs=model_kwargs,
         encode_kwargs=encode_kwargs
     )
-    vectorstore = Chroma.from_documents(documents=docs, embedding=hf_embeddings)    #Chroma.from_documents: Create a vectorstore from a list of documents
+    vectorstore = ChromaCollection.get_collection(
+        collection_name="catalog_indexing",
+        persist_directory="./chroma_catalog_indexing",
+        embedding_function=hf_embeddings,
+        documents=docs,
+    )  # Chroma.from_documents: Create a vectorstore from a list of documents
     return vectorstore
--- src/vector_stores/chroma.py
+++ src/vector_stores/chroma.py
@@ -1,9 +1,9 @@
-import chromadb
 import logging
 from typing import Optional
+
 from langchain_chroma import Chroma
-from langchain_core.embeddings import Embeddings
 from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
 
 logger = logging.getLogger(__name__)
 
@@ -12,12 +12,12 @@ class ChromaCollection:
 
     @classmethod
     def get_collection(
-        cls,
-        collection_name: str,
-        persist_directory: Optional[str] = None,
-        embedding_function: Optional[Embeddings] = None,
-        documents: Optional[list[Document]] = None,
-        override_collection_if_exists: Optional[bool] = False,
+            cls,
+            collection_name: str,
+            persist_directory: Optional[str] = None,
+            embedding_function: Optional[Embeddings] = None,
+            documents: Optional[list[Document]] = None,
+            override_collection_if_exists: Optional[bool] = False,
     ) -> Chroma:
 
         if override_collection_if_exists and documents is not None:
@@ -36,9 +36,9 @@ class ChromaCollection:
                 f"requested collection not found at path: {persist_directory}, creating a new collection: {collection_name}"
             )
 
-        if documents is None:
-            raise ValueError("can't create an empty collection")
+            if documents is None:
+                raise ValueError("can't create an empty collection")
 
-        return Chroma.from_documents(
-            documents, embedding_function, collection_name=collection_name, persist_directory=persist_directory
-        )
+            return Chroma.from_documents(
+                documents, embedding_function, collection_name=collection_name, persist_directory=persist_directory
+            )
